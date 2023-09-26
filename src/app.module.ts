fd12c85ed5f2e80f3c801870d0dc3de879476120
Wesley <wesleyofficial@hotmail.com>
2023-09-26 14:45:41 -0300

    mudando app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegistroAguaController } from './registro-agua/registro-agua.controller';
+import { RegistroAguaService } from './registro-agua/registro-agua.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, RegistroAguaController],
+  providers: [AppService, RegistroAguaService],
 })
-export class AppModule {}
+export class AppModule { }
