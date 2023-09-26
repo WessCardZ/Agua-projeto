392a2e4614747837619550d2354c16973d80e1a3
Wesley <wesleyofficial@hotmail.com>
2023-09-26 15:19:48 -0300

    adiciona todo o CRUD em Service

--- src/registro-agua/registro-agua.service.ts
+++ src/registro-agua/registro-agua.service.ts
@@ -3,9 +3,24 @@ import { RegistroAgua } from './interfaces/registro-agua.interface';
 
 @Injectable()
 export class RegistroAguaService {
-    private readonly registroAgua: RegistroAgua[] = [];
+    private readonly registrosAgua: RegistroAgua[] = [];
 
     findAll(): RegistroAgua[] {
-        return this.registroAgua;
+        return this.registrosAgua;
+    }
+    create(registroAgua: RegistroAgua): void {
+        this.registrosAgua.push(registroAgua)
+    }
+    findById(id: number): RegistroAgua {
+        return this.registrosAgua.find(registroAgua => registroAgua.id === id)
+    }
+    findIndexById(id: number): number {
+        return this.registrosAgua.findIndex(registroAgua => registroAgua.id === id)
+    }
+    deleteByIndex(index: number): void {
+        this.registrosAgua.splice(index, 1)
+    }
+    update(index: number, registroAgua: RegistroAgua): void {
+        this.registrosAgua.splice(index, 1, registroAgua)
     }
 }
