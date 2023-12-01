ebb9e8952ad8ec0e5350ca9be8c7678a5300779b
WessCardZ <wesleyofficial@hotmail.com>
2023-12-01 19:56:18 -0300

    Rotas usuario para Delete,Post e Update

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Res, Post, Put } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { Usuario } from './entities/usuario.entity';
+import { Response } from 'express';
+import { CreateUsuarioOrUpdateDto } from './dto/create-usuario.dto';
 
 @Controller('usuario')
 export class UsuarioController {
@@ -9,7 +11,43 @@ export class UsuarioController {
 
     @Get()
     findAll(): Promise<Usuario[]> {
+        console.log('Ola')
         return this.usuarioService.findAll();
     }
 
+    @Get(':id')
+    async findOne(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+        const usuarioDado = await this.usuarioService.findById(id);
+        if (usuarioDado) {
+            res.status(HttpStatus.OK).json(usuarioDado)
+        }
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+        const indexUsuarioDado = await this.usuarioService.findById(id);
+        if (indexUsuarioDado) {
+            await this.usuarioService.remove(id)
+            res.status(HttpStatus.NO_CONTENT).send()
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send()
+        }
+    }
+
+    @Post()
+    async create(@Body() dto: CreateUsuarioOrUpdateDto, @Res() res: Response) {
+        const criarUsuario = await this.usuarioService.save(dto);
+        res.status(HttpStatus.CREATED).json(criarUsuario)
+    }
+
+    @Put(':id')
+    async update(@Param('id', ParseIntPipe) id: number, @Body() dto: CreateUsuarioOrUpdateDto, @Res() res: Response) {
+        const usuarioDado = await this.usuarioService.findById(id);
+        if (usuarioDado) {
+            await this.usuarioService.update(id, dto);
+            res.status(HttpStatus.NO_CONTENT).send();
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
 }
