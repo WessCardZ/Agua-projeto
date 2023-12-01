fec65c547cdb3a9a0b4368c8e2d4e4ed392c7caa
WessCardZ <wesleyofficial@hotmail.com>
2023-12-01 19:44:27 -0300

    Usuario Service Save,Update

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Usuario } from './entities/usuario.entity';
+import { CreateUsuarioOrUpdateDto } from './dto/create-usuario.dto';
 
 @Injectable()
 export class UsuarioService {
@@ -14,8 +15,16 @@ export class UsuarioService {
         return this.usuarioRepository.find();
     }
 
+    save(dto: CreateUsuarioOrUpdateDto): Promise<Usuario> {
+        return this.usuarioRepository.save(dto)
+    }
+
     findById(id: number): Promise<Usuario | null> {
         return this.usuarioRepository.findOneBy({ id });
+    }
+
+    async update(id: number, dto: CreateUsuarioOrUpdateDto): Promise<void> {
+        await this.usuarioRepository.update(id, dto)
     }
 
     async remove(id: number): Promise<void> {
